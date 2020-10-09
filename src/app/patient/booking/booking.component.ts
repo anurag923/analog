731595a48524afefbb145fcc36262d4987aabcc8
“rebba” <rebba@gmail.com>
2020-10-09 15:42:09 +0530

    I have designed booking active button

--- src/app/patient/booking/booking.component.ts
+++ src/app/patient/booking/booking.component.ts
@@ -1,4 +1,7 @@
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
 
 @Component({
   selector: 'app-booking',
@@ -10,6 +13,12 @@ export class BookingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+      $('.btn1').click(function(){
+        $('.btn1').removeClass('active1').addClass('inactive1');
+         $(this).removeClass('inactive1').addClass('active1');
+        });
+
   }
 
 }
