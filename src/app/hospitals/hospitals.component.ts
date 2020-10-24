90d013240ea3d7579d295baa26be72357fdc5943
“rebba” <rebba@gmail.com>
2020-10-24 11:19:44 +0530

    add hospitals component and images

--- src/app/hospitals/hospitals.component.ts
+++ src/app/hospitals/hospitals.component.ts
@@ -20,7 +20,7 @@ export class HospitalsComponent implements OnInit {
       $(".toggle").toggleClass("active");
     });
 }
-  customOptions: OwlOptions = {
+  customOptions1: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
@@ -40,6 +40,32 @@ export class HospitalsComponent implements OnInit {
       },
       940: {
         items: 3
+      }
+    },
+    nav: true
+  }
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
       }
     },
     nav: true
